21032db705191770b3b67a92ac114061c3555e06
Alejpg05 <137833794+Alejpg05@users.noreply.github.com>
2023-09-07 19:45:59 -0300

    Add files via upload

---
+++ ejercicio3.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio3</title>
+</head>
+<body>
+    <h1>Ejercicio 3</h1>
+    <?php
+    $numero1=2;
+    $numero2=10;
+    $suma=$numero1+$numero2;
+    $resta=$numero1-$numero2;
+    $division=$numero1/$numero2;
+    $resto=$numero1%$numero2;
+        echo "<p>Los numeros son $numero1 y $numero2, el resultado de la suma es $suma, la resta $resta, la división es $division y el resto es $resto.</p>\n";
+    ?>
+</body>
+</html>
---
+++ ejercicio4y5.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 4 y 5</title>
+</head>
+<body>
+    <?php
+    $gradosCelsius=20;
+    $conversionFarenheit=$gradosCelsius*1.8+32;
+        echo "$gradosCelsius grados celsius pasados a grados Farenheit son $conversionFarenheit grados.";
+    ?>
+</body>
+</html>
